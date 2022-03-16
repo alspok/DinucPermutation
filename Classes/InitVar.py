458af66497f81f532f8ce2601a8f1af886362302
alvydas spokauskas <alspok@gmail.com>
2022-03-16 18:14:24 +0200

    .

--- Classes/InitVar.py
+++ Classes/InitVar.py
@@ -1,14 +1,19 @@
+import math
+
 """Init var for project"""
 class InitVar():
     nucList = ['a', 'c', 'g', 't']
+    fact_nucList = math.factorial(int(len(nucList)))
 
     dinucList = ['tt', 'ac', 'ag', 'at',
                  'ca', 'cc', 'cg', 'ct',
                  'ga', 'gc', 'gg', 'gt',
                  'ta', 'tc', 'tg', 'aa'
     ]
+    fact_dinucList = math.factorial(int(len(dinucList)))
     
     dinucList_aa = ['aa', 'aa', 'aa', 'aa', 'aa', 'aa', ]
+    fact_dinucList_aa = math.factorial(len(dinucList_aa))
     
     dinucDict = {'aa': [0, 0, 0], 'ac': [0, 0, 0], 'ag': [0, 0, 0], 'at': [0, 0, 0],
                  'ca': [0, 0, 0], 'cc': [0, 0, 0], 'cg': [0, 0, 0], 'ct': [0, 0, 0],
