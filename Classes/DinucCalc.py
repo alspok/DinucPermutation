210b0b087c9bce74cb62fa677ac1cc60dd55ae7f
alvydas spokauskas <alspok@gmail.com>
2022-03-20 20:43:17 +0200

    .

--- Classes/DinucCalc.py
+++ Classes/DinucCalc.py
@@ -3,26 +3,27 @@ from Classes.InitVar import InitVar as iv
 class DinucCalc():
         
     def dinucCalc(self, diList: list) -> float:
-        diDict = iv.dinucDict
         diString = ''.join(diList)
         if len(diString) % 2 == 0:
             diString = diString + diString[0]
-        diLength = len(diString) // 2
-            
-        for key, value in diDict.items():
+        
+        diDict = {'aa': [0, 0, 0], 'ac': [0, 0, 0], 'ag': [0, 0, 0], 'at': [0, 0, 0],
+                  'ca': [0, 0, 0], 'cc': [0, 0, 0], 'cg': [0, 0, 0], 'ct': [0, 0, 0],
+                  'ga': [0, 0, 0], 'gc': [0, 0, 0], 'gg': [0, 0, 0], 'gt': [0, 0, 0],
+                  'ta': [0, 0, 0], 'tc': [0, 0, 0], 'tg': [0, 0, 0], 'tt': [0, 0, 0]
+        }
+        
+        for key in diDict.keys():
             indices = [index for index in range(len(diString)) if diString.startswith(key, index)]
             for index in indices:
                 if index % 2 == 0:
                     diDict[key][0] += 1
                 else:
                     diDict[key][1] += 1
-            diDict[key][0] = diDict[key][0] / diLength
-            diDict[key][1] = diDict[key][1] / diLength
             diDict[key][2] = abs(diDict[key][0] - diDict[key][1])
             
         diDiffSum = 0
         for value in diDict.values():
             diDiffSum += value[2]
-        # diDict = {}
-        
-        return diDiffSum
+            
+        return diDict, diDiffSum
--- DinucPerm.py
+++ DinucPerm.py
@@ -1,4 +1,5 @@
 import math
+from ntpath import join
 from Classes.InitVar import InitVar as iv
 from Classes.Permutarion import Permutation as pm
 from Classes.DinucCalc import DinucCalc 
@@ -12,14 +13,38 @@ def dinucPermut():
     i = 1
     for item in result:
         diCalc = DinucCalc()
-        dinucDiffSum = diCalc.dinucCalc(item)
+        _, dinucDiffSum = diCalc.dinucCalc(item)
         if dinucDiffSum == 0:
             print(f"{i}\t{list(item)}")
         elif i % 10000 == 0:
             print(f"{i} of {fact_nuc_list} {(i/fact_nuc_list) * 100:2.6f} %", end='\r')
         i += 1
+    print()
+    pass
+
+def dinucPermutCalc():
+    nuc_list = iv.dinucList
+    dinuc_list_length = len(''.join(nuc_list)) // 2
+    result = pm.permutations(nuc_list)
     
-    input("\nPress any key to exit")
-             
+    dinucDictSum = iv.dinucDictSum
+    for item in result:
+        diDictionary, _ = DinucCalc().dinucCalc(item)
+        for key in diDictionary.keys():
+            dinucDictSum[key][0] += diDictionary[key][0]
+            dinucDictSum[key][1] += diDictionary[key][1]
+            dinucDictSum[key][2] += abs(diDictionary[key][0] - diDictionary[key][1])
+            dinucDictSum[key][3] = diDictionary[key][0] / dinuc_list_length
+            dinucDictSum[key][4] = diDictionary[key][1] / dinuc_list_length 
+            dinucDictSum[key][5] = abs(dinucDictSum[key][3] - dinucDictSum[key][4])
+    
+    for key, value in dinucDictSum.items():
+        if any(value):
+            print(f"{key}\t{value[0]}\t{value[1]}\t{value[2]}\t{value[3]:.6f}\t{value[4]:.6f}\t{value[5]:.6f}")
+    print (f"\n{math.factorial(int(len(nuc_list)))} permutations")
+            
+    pass
+         
 if __name__ == '__main__':
-    dinucPermut()
+    # dinucPermut()
+    dinucPermutCalc()
