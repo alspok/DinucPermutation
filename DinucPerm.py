fe1fb75181afbf7e999fcedb1990aa5b46b94018
alvydas spokauskas <alspok@gmail.com>
2022-03-22 01:08:18 +0200

    .

--- DinucPerm.py
+++ DinucPerm.py
@@ -1,3 +1,4 @@
+import copy
 import math
 from ntpath import join
 from Classes.InitVar import InitVar as iv
@@ -27,21 +28,30 @@ def dinucPermutCalc():
     dinuc_list_length = len(''.join(nuc_list)) // 2
     result = pm.permutations(nuc_list)
     
-    dinucDictSum = iv.dinucDictSum
     for item in result:
+        dinucFull = copy.deepcopy(iv.dinucDictFull)
         diDictionary, _ = DinucCalc().dinucCalc(item)
         for key in diDictionary.keys():
-            dinucDictSum[key][0] += diDictionary[key][0]
-            dinucDictSum[key][1] += diDictionary[key][1]
-            dinucDictSum[key][2] += abs(diDictionary[key][0] - diDictionary[key][1])
-            dinucDictSum[key][3] = diDictionary[key][0] / dinuc_list_length
-            dinucDictSum[key][4] = diDictionary[key][1] / dinuc_list_length 
-            dinucDictSum[key][5] = abs(dinucDictSum[key][3] - dinucDictSum[key][4])
-    
-    for key, value in dinucDictSum.items():
-        if any(value):
-            print(f"{key}\t{value[0]}\t{value[1]}\t{value[2]}\t{value[3]:.6f}\t{value[4]:.6f}\t{value[5]:.6f}")
-    print (f"\n{math.factorial(int(len(nuc_list)))} permutations")
+            dinucFull[key][0] += diDictionary[key][0]
+            dinucFull[key][1] += diDictionary[key][1]
+            dinucFull[key][2] += abs(diDictionary[key][0] - diDictionary[key][1])
+            dinucFull[key][3] = diDictionary[key][0] / dinuc_list_length
+            dinucFull[key][4] = diDictionary[key][1] / dinuc_list_length
+            dinucFull[key][5] = abs(dinucFull[key][3] - dinucFull[key][4])
+
+        dinucSeq = ''.join(item)
+        dinucSeq1st = dinucSeq
+        dinucSeq2nd = dinucSeq[1:] + dinucSeq[0]
+        print(f"{dinucSeq1st}")
+        print(f" {dinucSeq2nd}")
+        
+        for key, value in dinucFull.items():
+            print(f"{key}\t{dinucFull[key][0]}\t{dinucFull[key][1]}\t{dinucFull[key][2]}\t{dinucFull[key][3]:.6f}\t{dinucFull[key][4]:.6f}\t{dinucFull[key][5]:.6f}")
+        pass
+    # for key, value in dinucFull.items():
+    #     if any(value):
+    #         print(f"{key}\t{value[0]}\t{value[1]}\t{value[2]}\t{value[3]:.6f}\t{value[4]:.6f}\t{value[5]:.6f}")
+    # print (f"\n{math.factorial(int(len(nuc_list)))} permutations")
             
     pass
          
